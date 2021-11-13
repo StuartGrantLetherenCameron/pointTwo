94a308db2fe306f0e64b3da7cab3f6022b201dc7
StuartCameron98 <stuart@cronometer.com>
2021-11-12 21:21:34 -0800

    got more stuff working and it goes

--- venv/point2.py
+++ venv/point2.py
@@ -135,11 +135,24 @@ def export(building_list):
         sheet.insert_row(insert_row,2)
 
 
-    #pprint(data)
+def info():
+    scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open("bigDayta").sheet1
+
+    data = sheet.get_all_records()
+
+    print(len(data))
 
 if __name__ == '__main__':
     building_list = main()
     export(building_list)
+    # info()
 
 
 
